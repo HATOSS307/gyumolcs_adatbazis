b1fe1eb6d58b5956c876bfd457c8a17da87f6af9
HATOSS307 <hatoss307@gmail.com>
2022-11-14 11:39:29 +0100

    feladat

---
+++ gyumolcs_adatbazis/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace gyumolcs_adatbazis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list_adatok = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1_id = new System.Windows.Forms.TextBox();
+            this.textBox1_nev = new System.Windows.Forms.TextBox();
+            this.numericUpDown1_egysegar = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1_mennyiseg = new System.Windows.Forms.NumericUpDown();
+            this.data = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1_egysegar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1_mennyiseg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // list_adatok
+            // 
+            this.list_adatok.Dock = System.Windows.Forms.DockStyle.Left;
+            this.list_adatok.FormattingEnabled = true;
+            this.list_adatok.Location = new System.Drawing.Point(0, 0);
+            this.list_adatok.Name = "list_adatok";
+            this.list_adatok.Size = new System.Drawing.Size(182, 314);
+            this.list_adatok.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.data);
+            this.groupBox1.Controls.Add(this.numericUpDown1_mennyiseg);
+            this.groupBox1.Controls.Add(this.numericUpDown1_egysegar);
+            this.groupBox1.Controls.Add(this.textBox1_nev);
+            this.groupBox1.Controls.Add(this.textBox1_id);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(182, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 314);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Az adatok";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Neve:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Egységár:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mennyiség:";
+            // 
+            // textBox1_id
+            // 
+            this.textBox1_id.Location = new System.Drawing.Point(96, 52);
+            this.textBox1_id.Name = "textBox1_id";
+            this.textBox1_id.Size = new System.Drawing.Size(84, 20);
+            this.textBox1_id.TabIndex = 4;
+            // 
+            // textBox1_nev
+            // 
+            this.textBox1_nev.Location = new System.Drawing.Point(96, 81);
+            this.textBox1_nev.Name = "textBox1_nev";
+            this.textBox1_nev.Size = new System.Drawing.Size(84, 20);
+            this.textBox1_nev.TabIndex = 5;
+            // 
+            // numericUpDown1_egysegar
+            // 
+            this.numericUpDown1_egysegar.Location = new System.Drawing.Point(97, 114);
+            this.numericUpDown1_egysegar.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1_egysegar.Name = "numericUpDown1_egysegar";
+            this.numericUpDown1_egysegar.Size = new System.Drawing.Size(83, 20);
+            this.numericUpDown1_egysegar.TabIndex = 6;
+            // 
+            // numericUpDown1_mennyiseg
+            // 
+            this.numericUpDown1_mennyiseg.Location = new System.Drawing.Point(97, 154);
+            this.numericUpDown1_mennyiseg.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1_mennyiseg.Name = "numericUpDown1_mennyiseg";
+            this.numericUpDown1_mennyiseg.Size = new System.Drawing.Size(83, 20);
+            this.numericUpDown1_mennyiseg.TabIndex = 7;
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(57, 214);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(165, 34);
+            this.data.TabIndex = 8;
+            this.data.Text = "Rögzítés";
+            this.data.UseVisualStyleBackColor = true;
+            this.data.Click += new System.EventHandler(this.data_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 314);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.list_adatok);
+            this.Name = "Form1";
+            this.Text = "Gyümölcsök";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1_egysegar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1_mennyiseg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list_adatok;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1_mennyiseg;
+        private System.Windows.Forms.NumericUpDown numericUpDown1_egysegar;
+        private System.Windows.Forms.TextBox textBox1_nev;
+        private System.Windows.Forms.TextBox textBox1_id;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button data;
+    }
+}
+
---
+++ gyumolcs_adatbazis/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace gyumolcs_adatbazis
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connect = null;
+        MySqlCommand cmd = null;
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void data_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1_nev.Text))
+            {
+                MessageBox.Show("Adja meg a nevet");
+                textBox1_nev.Focus();
+                return;
+            }
+            if (numericUpDown1_egysegar.Value < 0 )
+            {
+                MessageBox.Show("Érvénytelen egységár!");             
+                numericUpDown1_mennyiseg.Focus();
+                return;
+            }
+            if (numericUpDown1_mennyiseg.Value < 0)
+            {
+                MessageBox.Show("Érvénytelen mennyiésg");
+                numericUpDown1_mennyiseg.Focus();
+                return;
+            }
+            cmd.CommandText = "INSERT INTO `gyümölcsök` (`id`, `nev`, `egysegar`, `mennyiseg`) VALUES (NULL, @nev, @egysegar, @mennyiseg);";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@nev", textBox1_nev.Text);
+            cmd.Parameters.AddWithValue("@egysegar", numericUpDown1_egysegar.Text);
+            cmd.Parameters.AddWithValue("@mennyiseg", numericUpDown1_mennyiseg.Text);
+            connect.Open();
+            try
+            {
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Sikeres adat rögzítés");
+                    textBox1_id.Text = "";
+                    textBox1_nev.Text = "";
+                    numericUpDown1_mennyiseg.Value = numericUpDown1_mennyiseg.Minimum;
+                    numericUpDown1_egysegar.Value = numericUpDown1_egysegar.Minimum;
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            connect.Close();
+            gyumolcs_list_update();
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "gyumolcsok";
+            connect = new MySqlConnection(builder.ConnectionString);
+            try
+            {
+                //terv szerint
+                connect.Open();
+                cmd = connect.CreateCommand();
+            }
+            catch (MySqlException ex)
+            {
+                //terv megbukott
+                MessageBox.Show(ex.Message + Environment.NewLine + "A program leált");
+                Environment.Exit(0);
+            }
+            finally
+            {
+                connect.Close();
+            }
+            gyumolcs_list_update();
+        }
+
+        private void gyumolcs_list_update()
+        {
+            list_adatok.Items.Clear();
+            cmd.CommandText = "SELECT `id`, `nev`,`egysegar`,`mennyiseg` FROM `gyümölcsök`";
+            connect.Open();
+            using (MySqlDataReader dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    gyumolcs uj = new gyumolcs(dr.GetInt32("Id"), dr.GetString("nev"), dr.GetDouble("egysegar"), dr.GetDouble("mennyiseg"));
+                    list_adatok.Items.Add(uj);
+                }
+            }
+            connect.Close();
+        }
+    }
+}
